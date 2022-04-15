7dac84dadebdda3de96128af9d4eb264ae1342fc
2018008722 <cria2000@naver.com>
2022-04-16 02:56:38 +0900

    구글태그관리자 innerHTML 변경

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -31,20 +31,15 @@ export const MainPage = (props: Props) => {
 
     useEffect(() => {
         const tagScript = document.createElement('script')
-        tagScript.src= 'https://www.googletagmanager.com/gtm.js?id='
+        tagScript.src= "https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID"
         tagScript.async = true
         tagScript.innerHTML = `
             function(w,d,s,l,i)
-            {
-                w[l]=w[l]||[];
-                w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});
-                var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-                j.async=true;
-                j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;
-                f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-5GC2QZP');`
+              window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'GA_MEASUREMENT_ID');`
 
         document.body.appendChild(tagScript)
         return () => {
