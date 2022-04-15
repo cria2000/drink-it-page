db2fe27b979390c25196791435ca66f765dd0f68
2018008722 <cria2000@naver.com>
2022-04-15 21:57:31 +0900

    모바일 화면 크기 맞춤

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Styles/css/App.css';
 import {MainPage} from "./MainPage";
 
 function App() {
+    function setScreenSize() {
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty("--vh", `${vh}px`);
+    }
+    useEffect(() => {
+        setScreenSize();
+    });
+    
   return (
     <div className="App">
        <MainPage />
